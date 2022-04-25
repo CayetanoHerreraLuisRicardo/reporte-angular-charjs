c794fe16851dcbbb79bec0072bc47ca0f800ae1a
Ricardo Cayetano <rcayetano@bioxor.com>
2022-04-25 04:24:32 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,128 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { SocialAuthService } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+import { HostListener } from "@angular/core";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reportes';
+export class AppComponent implements OnInit, AfterViewInit {
+  public barChartOptions: ChartOptions = {
+    responsive: true
+  };
+  public barChartLabels: Label[] = ['Compromiso de la dirección', 'Capacidades internas', 'Capacidad de exploración y relación con el exterior', 'Procesos y herramientas', 'Modelo de gestión'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartDataSector: ChartDataSets[] = [
+    {
+      data: [65, 59, 80, 81, 56], label: 'Grupo Pana', backgroundColor: '#52B9EA ', hoverBackgroundColor: "#00ABFC",
+      hoverBorderColor: "#E2EDFE", borderWidth: 1, borderColor: '#f2f7ff', hoverBorderWidth: 1,
+    },
+    {
+      data: [28, 48, 40, 19, 86], label: 'Sector', backgroundColor: ' #FAB163', hoverBackgroundColor: '#FF8400',
+      hoverBorderColor: "#E2EDFE", borderWidth: 1, borderColor: '#f2f7ff', hoverBorderWidth: 1,
+    }
+  ];
+  public barChartDataPais: ChartDataSets[] = [
+    {
+      data: [65, 59, 80, 81, 56], label: 'Grupo Pana', backgroundColor: '#52B9EA ', hoverBackgroundColor: "#00ABFC",
+      hoverBorderColor: "#E2EDFE", borderWidth: 1, borderColor: '#f2f7ff', hoverBorderWidth: 1,
+    },
+    {
+      data: [28, 48, 40, 19, 86], label: 'País', backgroundColor: ' #FAB163', hoverBackgroundColor: '#FF8400',
+      hoverBorderColor: "#E2EDFE", borderWidth: 1, borderColor: '#f2f7ff', hoverBorderWidth: 1,
+    }
+  ];
+  user: SocialUser = new SocialUser();
+  loggedIn: boolean = false;
+  scrHeight: any;
+  scrWidth: any;
+  showGraphs = false;
+  constructor(private authService: SocialAuthService) { }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event: any) {
+    this.scrHeight = window.innerHeight;
+    this.scrWidth = window.innerWidth;
+    this.barChartOptions = this.getConfig();
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
+      (succes) => {
+        console.log(succes);
+      },
+      (error) => {
+
+      }
+    )
+  }
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+  ngAfterViewInit(): void {
+    this.barChartOptions = this.getConfig();
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 2000);
+  }
+  getConfig(): ChartOptions {
+    const fontSize = this.getFontSize();
+    const options = {
+      responsive: true,
+      legend: {
+        labels: {
+          fontSize,
+          fontColor: '#7c8db5'
+        }
+      },
+      scales: {
+        yAxes: [
+          {
+            display: true,
+            ticks: {
+              fontSize,
+              fontColor: '#7c8db5'
+            }
+          }
+        ],
+        xAxes: [
+          {
+            display: true,
+            ticks: {
+              fontSize,
+              fontColor: '#7c8db5'
+            }
+          }
+        ],
+      }
+    };
+    return options;
+  }
+  getFontSize(): number {
+    if (window.innerWidth >= 1200) {
+      return 16;
+    } else if (window.innerWidth >= 992) {
+      return 14
+    } else if (window.innerWidth >= 768) {
+      return 12
+    } else if (window.innerWidth >= 576) {
+      return 10
+    } else {
+      return 6
+    }
+  }
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event: any) {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports: [BrowserModule, FormsModule, ChartsModule, SocialLoginModule, NgbModule],
+  declarations: [AppComponent, HelloComponent],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '5119129712-d8bnm1e64fdjlog8bu8ljbiplq2lncbk.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
